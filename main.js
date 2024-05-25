1cfe46efa60fc54d93c8218c27171c40286a624e
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2024-05-25 11:40:41 -0600

    Add ability to delete saved color palettes

--- main.js
+++ main.js
@@ -8,37 +8,37 @@ const noSavedPalettesText = document.getElementById("no-saved-palettes");
 const currentColors = [
   {
     hex: "#EA9999",
+    id: 0,
+    colorId: "color-0",
+    textId: "text-0",
+    isLocked: false,
+  },
+  {
+    hex: "#FACB9C",
     id: 1,
     colorId: "color-1",
     textId: "text-1",
     isLocked: false,
   },
   {
-    hex: "#FACB9C",
+    hex: "#FFE59A",
     id: 2,
     colorId: "color-2",
     textId: "text-2",
     isLocked: false,
   },
   {
-    hex: "#FFE59A",
+    hex: "#B6D7A8",
     id: 3,
     colorId: "color-3",
     textId: "text-3",
     isLocked: false,
   },
   {
-    hex: "#B6D7A8",
+    hex: "#A4C4CA",
     id: 4,
     colorId: "color-4",
     textId: "text-4",
-    isLocked: false,
-  },
-  {
-    hex: "#A4C4CA",
-    id: 5,
-    colorId: "color-5",
-    textId: "text-5",
     isLocked: false,
   },
 ];
@@ -97,7 +97,7 @@ function randomHex() {
 }
 
 function lockColor(lockButton) {
-  const buttonId = lockButton.id.substring(lockButton.id.indexOf("-") + 1);
+  const buttonId = lockButton.id.substring(lockButton.id.indexOf("-"));
   currentColors.forEach((color) => {
     if (color.id.toString() === buttonId) {
       color.isLocked = !color.isLocked;
@@ -122,13 +122,19 @@ function saveCurrentPalette() {
 }
 
 function renderSavedPalettes() {
+  const savedPalettesContainer = document.querySelector(
+    ".saved-color-palettes"
+  );
+  savedPalettesContainer.innerHTML = `<p id="no-saved-palettes">No palettes saved yet!</p>`;
+
   if (savedPalettes.length > 0) {
+    savedPalettesContainer.innerHTML = "";
     noSavedPalettesText.hidden = true;
 
-    savedPalettesContainer = document.querySelector(".saved-color-palettes");
-    savedPalettesContainer.innerHTML = "";
-
     savedPalettes.forEach((palette) => {
+      const paletteContainer = document.createElement("div");
+      paletteContainer.className = "palette-container";
+
       const savedPalette = document.createElement("div");
       savedPalette.className = "saved-palette";
       savedPalette.id = `saved-palette-${palette.id}`;
@@ -147,19 +153,33 @@ function renderSavedPalettes() {
         renderPaletteInMainView(paletteID);
       });
 
-      savedPalettesContainer.appendChild(savedPalette);
+      paletteContainer.appendChild(savedPalette);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.className = "delete-button";
+      deleteButton.innerHTML = "&times;";
+      deleteButton.onclick = () => deletePalette(palette.id);
+
+      paletteContainer.appendChild(deleteButton);
+      savedPalettesContainer.appendChild(paletteContainer);
     });
   } else {
     noSavedPalettesText.hidden = false;
   }
 }
 
+function deletePalette(id) {
+  savedPalettes = savedPalettes.filter((palette) => palette.id !== id);
+  renderSavedPalettes();
+}
+
 function renderPaletteInMainView(paletteID) {
   var paletteObject = findSavedPaletteByID(paletteID);
+
   const colorsArray = paletteObject.hexColors;
   colorsArray.forEach((hexColor, index) => {
     var color = currentColors.find((currentColor) => {
-      return index + 1 === currentColor.id;
+      return index === currentColor.id;
     });
     color.hex = hexColor;
   });
